ad6e89a1fd15d93e7939e0aef4b82b4a0d9ffc04
shawnajean <shawnajean@users.noreply.github.com>
2016-02-03 21:01:21 -0600

    Flesh out the outline

--- catclicker.js
+++ catclicker.js
@@ -2,22 +2,51 @@ $(function() {
 
   var model = {
 
+    init: function() {
 
+    },
+    add: function() {
+
+    },
+    click: function( index ) {
+
+    }
   };
 
   var octopus = {
     init: function() {
+      model.init();
+      listView.init();
+      playView.init();
+    },
+    clickList: function() {
+
+    },
+    clickCat: function() {
+
+    },
+    render: function() {
 
     }
   };
 
   var listView = {
+    init: function() {
 
+    },
+    render: function() {
+
+    }
 
   };
 
   var playView = {
+    init: function() {
 
+    },
+    render: function( cat ) {
+
+    }
 
   };
 
