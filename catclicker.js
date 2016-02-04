f1ee3578bc7cb4543856bffc31fd7e3b52524ed9
shawnajean <shawnajean@users.noreply.github.com>
2016-02-03 21:18:43 -0600

    Implement octopus.getCats

--- catclicker.js
+++ catclicker.js
@@ -14,6 +14,9 @@ $(function() {
       this.cats.push( {url: "https://farm6.staticflickr.com/5704/20443802614_d0b0c0b5c5_b.jpg",
       name: "Yoann"} );
     },
+    getAllCats: function() {
+
+    },
     click: function( index ) {
 
     }
@@ -26,7 +29,7 @@ $(function() {
       catView.init();
     },
     getCats: function() {
-
+      return model.getAllCats();
     },
     clickList: function() {
 
